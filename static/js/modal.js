fa24386b9a58b07686e49c3bee38adf3190ccbf5
quelzinha-git <raqueltb76@gmail.com>
2026-08-19 11:20:28 -0300

    aula 1908

---
+++ static/js/modal.js
@@ -0,0 +1,19 @@
+function abrirModal() {
+    const modal = document.getElementById('meuModal');
+    if (modal) modal.showModal();
+}
+
+function fecharModal() {
+    const modal = document.getElementById('meuModal');
+    if (modal) modal.close();
+}
+
+function abrirModalCadastro() {
+    const modal = document.getElementById('modalCadastro');
+    if (modal) modal.showModal();
+}
+
+function fecharModalCadastro() {
+    const modal = document.getElementById('modalCadastro');
+    if (modal) modal.close();
+}
